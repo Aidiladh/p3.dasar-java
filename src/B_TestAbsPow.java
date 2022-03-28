155f98991019c5d5cb2ac289345b691920b7477f
Aidiladh <aidila1684211@gmail.com>
2022-03-28 15:16:52 +0700

    mengenal dan mengimplementasikan : •	Fungsi Math •	Array •	String •	Collection (ArrayList  HashMap)

---
+++ src/B_TestAbsPow.java
@@ -0,0 +1,15 @@
+import static java.lang.Math.abs ;
+import static java.lang.Math.pow ;
+import static java.lang .Math.sqrt;
+
+public class TestAbsPow {
+
+public static void main(String[] args) {
+//abs() and signum()
+        //sin(), cos(), tan(), acos(), asin(), atan() I/pow() and sqrt()
+        System.out.println("Nilai absolut -5 adalah " + abs(-5) );
+        System.out.println("Nilai 2 pangkat 3 adalah " + pow(2, 3));
+        System.out.println("Nilai 8 pangkat 1/3 adalah 11 + pow(8, (1 / 3.0))); " +sqrt(64));
+        System.out.println("Akar kuadrat dari 64 adalah " + sqrt(64) );
+}
+        }
---
+++ src/D_TestRadDegreeSinCos.java
@@ -0,0 +1,23 @@
+import static java.lang.Math.PI;
+import static java.lang.Math.cos ;
+import static java.lang.Math.sin ;
+import static java.lang.Math.toDegrees;
+import static java.lang.Math.toRadians;
+
+public class D_TestRadDegreeSinCos{
+    public static void main(String[] args) {
+//value of Pl in degrees
+        double rad = PI;
+        System.out.println(toDegrees(rad));
+//value of Pl/2 in degrees
+        rad = PI / 2;
+        System.out.println(toDegrees(rad));
+//value of PI/3 in degrees
+        rad = PI / 3;
+        System.out.println(toDegrees(rad));
+//value of Pl/4 in degrees
+        rad = PI / 4;
+        System.out.println(toDegrees(rad));
+        System.out.println("Sin 90 derajat adalah " + sin(toRadians(90)));
+        System.out.println("Cos 60 derajat adalah " + cos(toDegrees(60)));
+    }}
